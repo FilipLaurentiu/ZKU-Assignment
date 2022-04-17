fb294330b191fd68cd0d152a3c0f8b09a151af8e
Filip Laurentiu <f.laurentiu26@gmail.com>
2022-04-17 18:29:54 +0300

    Add documentation to the contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.0;
 
 contract HelloWorld {
+    /// @notice storage variable used to store the number
     uint private storedNumber;
 
-
+    /// @notice external function used to store a number in the contract
+    /// @param _number the uint256 number to be stored
     function storeNumber(uint _number) external {
         storedNumber = _number;
     }
 
+    /// @notice external function that return the stored number from the contract
     function retrieveNumber() external view returns(uint){
         return storedNumber;
     }
